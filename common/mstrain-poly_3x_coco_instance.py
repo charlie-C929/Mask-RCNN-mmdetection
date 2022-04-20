73065c9789fb7303a3cd9284485d09535c7c9578
charlie_c929 <charlie_C929@163.com>
2022-04-20 16:42:28 +0800

    configs modified

--- common/mstrain-poly_3x_coco_instance.py
+++ common/mstrain-poly_3x_coco_instance.py
@@ -1,9 +1,9 @@
 _base_ = '../_base_/default_runtime.py'
-# dataset settings
-dataset_type = 'CocoDataset'
+# dataset settings  这三行不用动
+dataset_type = 'CocoDataset'   # 数据集的格式
 data_root = 'data/coco/'
 img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)  # 归一化，官方提供，不用动
 
 # In mstrain 3x config, img_scale=[(1333, 640), (1333, 800)],
 # multiscale_mode='range'
@@ -40,13 +40,13 @@ test_pipeline = [
             dict(type='Collect', keys=['img']),
         ])
 ]
-
+#---------------------------------
 dataset_type = 'CocoDataset'
-classes=('person',)
+classes=('person','car','cat','dog',) # 单分类，可以改成自己的
 
-# Use RepeatDataset to speed up training
+# Use RepeatDataset to speed up training数据进入网络的操作
 data = dict(
-    samples_per_gpu=2,
+    samples_per_gpu=2,  # 每个gpu运算多少样本，越大效率越高
     workers_per_gpu=2,
     train=dict(
         classes=classes,
@@ -67,7 +67,7 @@ data = dict(
         ann_file=data_root + 'annotations/instances_val2017.json',
         img_prefix=data_root + 'val2017/',
         pipeline=test_pipeline))
-evaluation = dict(interval=1, metric=['bbox', 'segm'])
+evaluation = dict(interval=1, metric=['bbox', 'segm']) # 评价指标是'bbox', 'segm'，interval=1意思是每一步都需要评价
 
 # optimizer
 optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
