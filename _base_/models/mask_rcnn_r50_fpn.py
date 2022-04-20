f3e298d1096da0f091ddd7e78b4635898848b772
charlie_c929 <charlie_C929@163.com>
2022-04-20 16:32:32 +0800

    configs modify

--- _base_/models/mask_rcnn_r50_fpn.py
+++ _base_/models/mask_rcnn_r50_fpn.py
@@ -1,27 +1,27 @@
 # model settings
 model = dict(
-    type='MaskRCNN',
-    backbone=dict(
-        type='ResNet',
-        depth=50,
-        num_stages=4,
-        out_indices=(0, 1, 2, 3),
-        frozen_stages=1,
-        norm_cfg=dict(type='BN', requires_grad=True),
+    type='MaskRCNN',  # 名字可以自己起
+    backbone=dict(    # 网络骨架
+        type='ResNet',   # ResNet是别人提出的50层，卷积池化的过程，就是提取特征
+        depth=50,        # 所以是50层
+        num_stages=4,    # 提取特征过程中有4个阶段,把特征提取出来。
+        out_indices=(0, 1, 2, 3),  # 给4个提取阶段标了个号
+        frozen_stages=1,           # 把上面那个标号1的stage冻结了，因为打开的话消耗资源
+        norm_cfg=dict(type='BN', requires_grad=True),  # BN'层去掉一些东西
         norm_eval=True,
         style='pytorch',
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
-    neck=dict(
+        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),  # Pretrained是预训练的权重，下载地址是后面这个
+    neck=dict(                # 特征提取之后那四个，输入到'FPN'，得到五个输出。以前没这个部分，就是为了得到不同大小的特征
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
         out_channels=256,
         num_outs=5),
-    rpn_head=dict(
+    rpn_head=dict(            # 得到的五个输出，之后去区域把它框出来。基本不需要改变
         type='RPNHead',
         in_channels=256,
         feat_channels=256,
         anchor_generator=dict(
-            type='AnchorGenerator',
+            type='AnchorGenerator',     # AnchorGenerator是描点框，大概会画1000个框
             scales=[8],
             ratios=[0.5, 1.0, 2.0],
             strides=[4, 8, 16, 32, 64]),
@@ -29,10 +29,10 @@ model = dict(
             type='DeltaXYWHBBoxCoder',
             target_means=[.0, .0, .0, .0],
             target_stds=[1.0, 1.0, 1.0, 1.0]),
-        loss_cls=dict(
+        loss_cls=dict(                                                    # 画框的loss值计算出来
             type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
         loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
-    roi_head=dict(
+    roi_head=dict(             # 把框画出来之后，把不同的矩阵变成一样的大小，得到了bbox和mask
         type='StandardRoIHead',
         bbox_roi_extractor=dict(
             type='SingleRoIExtractor',
@@ -44,7 +44,7 @@ model = dict(
             in_channels=256,
             fc_out_channels=1024,
             roi_feat_size=7,
-            num_classes=1,
+            num_classes=4,
             bbox_coder=dict(
                 type='DeltaXYWHBBoxCoder',
                 target_means=[0., 0., 0., 0.],
@@ -53,7 +53,7 @@ model = dict(
             loss_cls=dict(
                 type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
             loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
-        mask_roi_extractor=dict(
+        mask_roi_extractor=dict(                      # mask涂色部分的处理
             type='SingleRoIExtractor',
             roi_layer=dict(type='RoIAlign', output_size=14, sampling_ratio=0),
             out_channels=256,
@@ -63,15 +63,15 @@ model = dict(
             num_convs=4,
             in_channels=256,
             conv_out_channels=256,
-            num_classes=1,
+            num_classes=4,
             loss_mask=dict(
                 type='CrossEntropyLoss', use_mask=True, loss_weight=1.0))),
     # model training and testing settings
-    train_cfg=dict(
+    train_cfg=dict(   # 阈值，放宽效果变差
         rpn=dict(
             assigner=dict(
                 type='MaxIoUAssigner',
-                pos_iou_thr=0.7,
+                pos_iou_thr=0.7,  # 焦斌比
                 neg_iou_thr=0.3,
                 min_pos_iou=0.3,
                 match_low_quality=True,
@@ -108,10 +108,10 @@ model = dict(
             pos_weight=-1,
             debug=False)),
     test_cfg=dict(
-        rpn=dict(
+        rpn=dict(     #  rpn的相关设定，比如这个1000就是多少个框
             nms_pre=1000,
             max_per_img=1000,
-            nms=dict(type='nms', iou_threshold=0.7),
+            nms=dict(type='nms', iou_threshold=0.7),   # 'nms'是一个算法
             min_bbox_size=0),
         rcnn=dict(
             score_thr=0.05,
